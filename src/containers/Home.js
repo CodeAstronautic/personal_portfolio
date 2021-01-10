36b1b6071c4af559eab596179528ffac94fcff44
pooja mishra <mishrapooja8128@gmail.com>
2021-01-10 21:42:13 +0530

    updated code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -27,7 +27,7 @@ Mongodb, Javascript, etc 💻.
           ​Outside of code, I like to create in other ways. From <a href="https://medium.com/mpooja_15" target="_blank">blogging</a> to <a href="https://www.youtube.com/channel/UChfrVcFHC-mfoqrJHuOYt-Q" target="_blank">music-making</a>, <a href="http://sophiacioccaphotography.tumblr.com/" target="_blank">photography</a> to <a href="https://www.instagram.com/sidewalk_poetry/" target="_blank">guerilla sidewalk chalk street art</a>, I'm always trying new ways of expressing myself. I also like to meditate, talk to strangers, and run lifestyle experiments.
         </p> */}
         <p>
-           Previously, I've <a href="https://www.gracehopper.com/" target="_blank">taught code to women</a>, served in the Peace Corps, and management consulted for Fortune 100 companies. I also hold a degree in economics from the University of Pennsylvania.
+           Previously, I've <a href="https://www.gracehopper.com/" target="_blank">taught code to women</a>, served in the Peace Corps, and management consulted for Fortune 100 companies. 
         </p>
       </div>
     );
