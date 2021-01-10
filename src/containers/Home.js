dd200499262e288324f3d7722c918a7bfc5bbe69
pooja mishra <mishrapooja8128@gmail.com>
2021-01-10 21:29:05 +0530

    updated code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -23,9 +23,9 @@ Mongodb, Javascript, etc 💻.
         <p>
           I've worked throughout the stack, but I especially love building React UI. I currently build software for the newsroom at the New York Times.
         </p>
-        <p>
+        {/* <p>
           ​Outside of code, I like to create in other ways. From <a href="https://medium.com/mpooja_15" target="_blank">blogging</a> to <a href="https://www.youtube.com/channel/UChfrVcFHC-mfoqrJHuOYt-Q" target="_blank">music-making</a>, <a href="http://sophiacioccaphotography.tumblr.com/" target="_blank">photography</a> to <a href="https://www.instagram.com/sidewalk_poetry/" target="_blank">guerilla sidewalk chalk street art</a>, I'm always trying new ways of expressing myself. I also like to meditate, talk to strangers, and run lifestyle experiments.
-        </p>
+        </p> */}
         <p>
            Previously, I've <a href="https://www.gracehopper.com/" target="_blank">taught code to women</a>, served in the Peace Corps, and management consulted for Fortune 100 companies. I also hold a degree in economics from the University of Pennsylvania.
         </p>
